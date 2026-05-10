e7417689212d90c5bf2a5ce279d1125c4440527f
Pratine <salvador30@live.com.pt>
2026-05-10 21:23:51 +0100

    Update ipc.ts

--- electron/ipc.ts
+++ electron/ipc.ts
@@ -13,6 +13,7 @@ import { loadAppSettings, saveAppSettings } from './services/appSettings'
 import { lookupISIN } from './services/isinLookup'
 import { elapsedPeriods, applyPeriods } from './services/interest'
 import { calcInvestmentTotals } from './services/lotCalcs'
+import { computeBalanceDelta, toStoredAmount } from './services/transactionCalcs'
 import type { Frequency, InterestType, DebtType, DebtStatus } from './domainTypes'
 import type { Prisma } from '@prisma/client'
 
